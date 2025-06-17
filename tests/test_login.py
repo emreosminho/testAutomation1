ee40133432d7f0543bc469e1c8b6b2bb6e77099e
emreosminho <emreosmancanakci@gmail.com>
2025-06-17 17:07:54 +0300

    sec autoJenkinsguncel

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,8 +1,16 @@
 from selenium import webdriver
-import pytest
+from selenium.webdriver.chrome.options import Options
+import tempfile
 
 def test_google():
-    driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_argument(f"--user-data-dir={tempfile.mkdtemp()}")  # Benzersiz profil dizini
+
+    driver = webdriver.Chrome(options=options)
     driver.get("https://www.google.com")
     assert "Google" in driver.title
     driver.quit()
